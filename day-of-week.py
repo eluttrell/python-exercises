9c4c8ba9079a2a9683c99c9a5d9af9b7c8ef18c0
Margaret Allstrom <margaretallstrom@Margarets-MacBook-Pro.local>
2016-09-22 16:43:03 -0400

    Modify day-of-week, guessing-game, tip-calc, work-or-sleep python exercises.

--- day-of-week.py
+++ day-of-week.py
@@ -1,15 +1,3 @@
-day = int(raw_input('Day (0-6)'))
-if day == 0:
-    print "Sunday"
-elif day == 1:
-    print "Monday"
-elif day == 2:
-    print "Teusday"
-elif day == 3:
-    print "Wednesday"
-elif day == 4:
-    print "Thursday"
-elif day == 5:
-    print "Friday"
-else:
-    print "Saturday"
+day_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+day = int(raw_input("Please put in the day of the week as a number between 0 and 6, 0 being Sunday \n"))
+print "Today is %s." % day_of_week[day]
--- work-or-sleep.py
+++ work-or-sleep.py
@@ -1,8 +1,6 @@
-day = raw_input('Day (0-6)')
-day = int(float(day))
-if day == 0:
-    print "Sleep in"
-elif day == 6:
-    print "Sleep in"
+day_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+day = int(raw_input('Day (0-6)'))
+if day == 0 or day == 6:
+    print "Today is %s. Sleep in." % day_of_week[day]
 else:
-    print "Go to work"
+    print "Today is %s. Go to work." % day_of_week[day]
